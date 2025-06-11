e6544158d9f38be2fc0a25dadec81d85ba5ce0e8
Nicolas Colli <collinicolas01@gmail.com>
2025-06-11 15:55:39 -0300

    fix modificar alias y comision del viaje

--- controllers/payments.js
+++ controllers/payments.js
@@ -33,23 +33,12 @@ paymentsRouter.patch('/alias', async (request, response) => {
     return response.status(401).json({ error: 'Unauthorized' })
   }
 
-  const { alias } = request.body
+  const { value } = request.body
 
   //tiene que coincidir con alias1 o 2
   const payment = await Payment.findOne({})
 
-  const alias1 = payment.alias1
-  const alias2 = payment.alias2
-
-  if (alias !== alias1 && alias !== alias2) {
-    return response.status(400).json({ error: 'Invalid alias' })
-  }
-
-  if (alias === alias1) {
-    payment.alias = alias1
-  } else {
-    payment.alias = alias2
-  }
+  payment.alias = value
 
   await payment.save()
 
@@ -62,6 +51,19 @@ paymentsRouter.patch('/fee', async (request, response) => {
   if (!user.isAdministrator) {
     return response.status(401).json({ error: 'Unauthorized' })
   }
+
+  const payment = await Payment.findOne({})
+  console.log('payment', payment)
+
+  const newFee = request.body.value
+
+  console.log('request.body', request.body)
+
+  payment.fee = newFee
+
+  await payment.save()
+
+  response.status(200).end()
 })
 
 paymentsRouter.get('/', async (request, response) => {
@@ -83,11 +85,11 @@ paymentsRouter.patch('/alias1', async (request, response) => {
     return response.status(401).json({ error: 'Unauthorized' })
   }
 
-  const { alias1 } = request.body
+  const { value } = request.body
 
   const payment = await Payment.findOne({})
 
-  payment.alias1 = alias1
+  payment.alias1 = value
 
   await payment.save()
 
@@ -101,11 +103,11 @@ paymentsRouter.patch('/alias2', async (request, response) => {
     return response.status(401).json({ error: 'Unauthorized' })
   }
 
-  const { alias2 } = request.body
+  const { value } = request.body
 
   const payment = await Payment.findOne({})
 
-  payment.alias2 = alias2
+  payment.alias2 = value
 
   await payment.save()
 
